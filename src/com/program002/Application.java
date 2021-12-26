941bdb66b1a81ec72e06ede498ceafee1aa03a49
sb920502 <bhowmik920502@gmail.com>
2021-12-26 13:01:29 +0600

    program002 solved

--- src/com/program002/Application.java
+++ src/com/program002/Application.java
@@ -1,7 +1,23 @@
 package com.program002;
 
+import java.util.Scanner;
+
 class Application {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n = ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(" ");
+            }
+        }
+        Solution solution = new Solution();
+        System.out.print("\n" + solution.numIdenticalPairs(nums));
     }
 }
---
+++ src/com/program002/Solution.java
@@ -0,0 +1,15 @@
+package com.program002;
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (i < j && nums[i] == nums[j]) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
